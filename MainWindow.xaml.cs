3c45aa0c0e781b25acbede02ada7d200b9354ade
kevin meledjebanhoro <kevinmeledjebanhoro@ramses.ii>
2023-03-29 16:16:14 +0200

    La méthode ComboBox_SelectionChanged est déclenchée lorsque l'utilisateur sélectionne un élément dans une liste déroulante (ComboBox). La méthode vérifie si un élément est sélectionné dans une grille de données (DataGrid) nommée datagridService. Si un élément est sélectionné, la méthode ouvre une nouvelle fenêtre de dialogue (FenetreAjoutServiceOuFonction) qui permet à l'utilisateur de saisir des informations sur un nouveau service ou une nouvelle fonction. Si l'utilisateur valide les informations en cliquant sur un bouton OK, la méthode crée un nouvel objet Service ou Fonction et l'ajoute à une base de données nommée bddPersonnels.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using biblioBDDpersonel;
+using BddpersonnelContext;
+
+namespace appliBDDpersonel
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private C_BDD_personnel bddPersonnels = null;
+
+        public object Gestionnaire { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            try {
+
+                bddPersonnels = new C_BDD_personnel();
+                List<Service> services = bddPersonnels.getAllServices();
+                datagridService.ItemsSource = services;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Nom_Selected(object sender, RoutedEventArgs e)
+        {
+            TBNom.Visibility = Visibility;
+        }
+
+        private void Prénom_Selected(object sender, RoutedEventArgs e)
+        {
+            TBPrénom.Visibility = Visibility;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (datagridService.SelectedItem != null)
+            {
+                Service selectedService = (Service)datagridService.SelectedItem;
+                FenetreAjoutServiceOuFonction fenetreAjout = new FenetreAjoutServiceOuFonction(selectedService);
+                if (fenetreAjout.ShowDialog() == true)
+                {
+                    string nom = fenetreAjout.Nom;
+                    string description = fenetreAjout.Description;
+                    bool isService = fenetreAjout.IsService;
+
+                    if (isService)
+                    {
+                        Service newService = new Service { Nom = nom, Description = description };
+                        bddPersonnels.AddService(newService);
+                    }
+                    else
+                    {
+                        Fonction newFonction = new Fonction { Nom = nom, Description = description, Service = selectedService };
+                        bddPersonnels.AddFonction(newFonction);
+                    }
+
+                    bddPersonnels.SaveChanges();
+
+                    RefreshData();
+                }
+            }
+        }
+
+        private void RefreshData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}   
